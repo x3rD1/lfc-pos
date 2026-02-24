3b6c56b29afb7682a1371a38a2250378f013b1f7
Rexx <rexxalfaro@gmail.com>
2026-02-23 21:27:35 -0800

    Instantiate passport

---
+++ backend/lib/passport.js
@@ -0,0 +1,57 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const prisma = require("./prisma");
+const bcrypt = require("bcryptjs");
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        // Check if user does not exist
+        if (!user) {
+          return done(null, false, { message: "Invalid email or password" });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        // Check if entered password is incorrect
+        if (!match) {
+          return done(null, false, { message: "Invalid email or password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        profilePicture: true,
+        createdAt: true,
+      },
+    });
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
