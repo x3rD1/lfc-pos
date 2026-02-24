dba2bf5e3aeeecb60350a2cc44a9bcd78eb5ec6d
Rexx <rexxalfaro@gmail.com>
2026-02-23 22:37:35 -0800

    Mount routes and added session middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,53 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
+
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const pgSession = require("connect-pg-simple")(session);
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+
+require("../lib/passport");
+
 const app = express();
 
-app.use(express.json());
+app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.set("trust proxy", 1);
+const sessionMiddleware = session({
+  store: new pgSession({
+    conString: process.env.DATABASE_URL,
+    createTableIfMissing: true,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  proxy: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  },
 });
+
+app.use(sessionMiddleware);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const { requireAuth } = require("./modules/auth/auth.middleware");
+const authRoutes = require("./modules/auth/auth.routes");
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("App is listening to port 3000");
