9c0314dea18784ab3b979b828b6d446525ad6ba0
Rexx <rexxalfaro@gmail.com>
2026-02-25 19:21:16 -0800

    Add new edge cases

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -15,6 +15,20 @@ function errorHandler(err, req, res, next) {
         message: "Invalid email or password",
       });
 
+    case "ITEM_NOT_FOUND":
+      return res.status(404).json({
+        message: "Item not found",
+      });
+
+    case "PRICE_CANNOT_BE_NEGATIVE":
+      return res.status(400).json({ message: "Price cannot be negative" });
+
+    case "ORDER_DO_NOT_EXISTS":
+      return res.status(404).json({ message: "Create an order" });
+
+    case "USER_DO_NOT_EXISTS":
+      return res.status(404).json({ message: "User not found" });
+
     case "FORBIDDEN":
       return res.status(403).json({ message: "Forbidden action" });
   }
