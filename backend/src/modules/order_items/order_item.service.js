766c2581e7f3e540e38b2139feb4bcca53f39a6f
Rexx <rexxalfaro@gmail.com>
2026-02-25 19:19:22 -0800

    Removed select id true to access price etc. and wrap everything in transaction

--- backend/src/modules/order_items/order_item.service.js
+++ backend/src/modules/order_items/order_item.service.js
@@ -1,31 +1,41 @@
 const prisma = require("../../../lib/prisma");
 
 exports.create_order_item = async ({ itemId, orderId, ownerId }) => {
-  const order = await prisma.order.findUnique({ where: { id: orderId } });
-  if (!order) throw new Error("ORDER_DO_NOT_EXISTS");
+  return await prisma.$transaction(async (tx) => {
+    const order = await tx.order.findUnique({ where: { id: orderId } });
+    if (!order) throw new Error("ORDER_DO_NOT_EXISTS");
 
-  const item = await prisma.item.findUnique({
-    where: { id_ownerId: { id: itemId, ownerId } },
-    select: { id: true },
+    const item = await tx.item.findUnique({
+      where: { id_ownerId: { id: itemId, ownerId } },
+    });
+
+    if (!item) throw new Error("ITEM_NOT_FOUND");
+
+    // Checks if an order_item with itemId already exists, increment quantity by 1 if yes, create if not
+    const order_item = await tx.order_item.upsert({
+      where: { itemId_orderId: { itemId, orderId } },
+      update: { quantity: { increment: 1 } },
+      create: {
+        name_at_order: item.name,
+        price_at_order: item.price,
+        image_at_order: item.image,
+        quantity: 1,
+        itemId: item.id,
+        orderId,
+      },
+    });
+
+    await tx.order.update({
+      where: { id: orderId },
+      data: {
+        total: {
+          increment: item.price,
+        },
+      },
+    });
+
+    return order_item;
   });
-
-  if (!item) throw new Error("ITEM_NOT_FOUND");
-
-  // Checks if an order_item with itemId already exists, increment quantity by 1 if yes, create if not
-  const order_item = await prisma.order_item.upsert({
-    where: { itemId_orderId: { itemId, orderId } },
-    update: { quantity: { increment: 1 } },
-    create: {
-      name_at_order: item.name,
-      price_at_order: item.price,
-      image_at_order: item.image,
-      quantity: 1,
-      itemId: item.id,
-      orderId,
-    },
-  });
-
-  return order_item;
 };
 
 exports.update_order_item = async ({ itemId, orderId }) => {
@@ -35,7 +45,6 @@ exports.update_order_item = async ({ itemId, orderId }) => {
   const updated = await prisma.$transaction(async (tx) => {
     const existing = await tx.order_item.findUnique({
       where: { itemId_orderId: { itemId, orderId } },
-      select: { id: true },
     });
 
     if (!existing) throw new Error("ITEM_NOT_FOUND");
@@ -68,7 +77,7 @@ exports.delete_order_item = async ({ itemId, orderId }) => {
 
   if (!order_item) throw new Error("ITEM_NOT_FOUND");
 
-  await prisma.order_item.delete({
+  return await prisma.order_item.delete({
     where: { itemId_orderId: { itemId, orderId } },
   });
 };
